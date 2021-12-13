b2217f5148dbf4ec68680c20e8e7a6508054f04c
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2021-12-14 02:09:15 +0900

    Refactor the structure of code

--- Assets/DepthColorGrading/DepthColorGrading.cs
+++ Assets/DepthColorGrading/DepthColorGrading.cs
@@ -6,29 +6,27 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-namespace DepthColorGrading
-{
-    public sealed class DepthColorGrading : VolumeComponent, IPostProcessComponent
-    {
-        public ClampedFloatParameter blend       = new ClampedFloatParameter(0f, 0f, 1f);
+namespace DepthColorGrading {
+    public sealed class DepthColorGrading : VolumeComponent, IPostProcessComponent {
+        public ClampedFloatParameter blend       = new ClampedFloatParameter(0f,   0f, 1f);
         public ClampedFloatParameter skyBoxBlend = new ClampedFloatParameter(0.5f, 0f, 1f);
         public ClampedFloatParameter falloff     = new ClampedFloatParameter(0.1f, 0f, 1f);
 
-        public ClampedFloatParameter h0          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter s0          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter l0          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter gamma0      = new ClampedFloatParameter(0f, -1f, 1f);
-        public ColorParameter        tint0       = new ColorParameter(Color.white);
-        public ColorParameter        lift0       = new ColorParameter(Color.black);
+        public ClampedFloatParameter h0     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter s0     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter l0     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter gamma0 = new ClampedFloatParameter(0f, -1f, 1f);
+        public ColorParameter        tint0  = new ColorParameter(Color.white);
+        public ColorParameter        lift0  = new ColorParameter(Color.black);
 
-        public ClampedFloatParameter h1          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter s1          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter l1          = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter gamma1      = new ClampedFloatParameter(0f, -1f, 1f);
-        public ColorParameter        tint1       = new ColorParameter(Color.white);
-        public ColorParameter        lift1       = new ColorParameter(Color.black);
+        public ClampedFloatParameter h1     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter s1     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter l1     = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter gamma1 = new ClampedFloatParameter(0f, -1f, 1f);
+        public ColorParameter        tint1  = new ColorParameter(Color.white);
+        public ColorParameter        lift1  = new ColorParameter(Color.black);
 
-        public bool IsActive() => blend.value > 0f;
+        public bool IsActive()         => blend.value > 0f;
         public bool IsTileCompatible() => false;
     }
 }
--- Assets/DepthColorGrading/DepthColorGradingPass.cs
+++ Assets/DepthColorGrading/DepthColorGradingPass.cs
@@ -8,34 +8,34 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 namespace DepthColorGrading {
-    public sealed class DepthColorGradingPass : ScriptableRenderPass
-    {
+    public sealed class DepthColorGradingPass : ScriptableRenderPass {
         private const string ShaderName = "PostEffect/DepthColorGrading";
 
-        private static class ShaderId
-        {
-            public static readonly int MainTex           = Shader.PropertyToID("_MainTex");
-            public static readonly int TempBuf           = Shader.PropertyToID("_TempBuf");
-            public static readonly int Lut0              = Shader.PropertyToID("_DepthColorGradingLut0");
-            public static readonly int Lut1              = Shader.PropertyToID("_DepthColorGradingLut1");
-            public static readonly int Params            = Shader.PropertyToID("_DepthColorGradingParams");
-            public static readonly int Params2           = Shader.PropertyToID("_DepthColorGradingParams2");
-            public static readonly int Params3           = Shader.PropertyToID("_DepthColorGradingParams3");
-            public static readonly int DepthToViewParams = Shader.PropertyToID("_DepthColorGradingDepthToViewParams");
+        private static class ShaderId {
+            private static         int P(string s) => Shader.PropertyToID(s);
+            public static readonly int MainTex           = P("_MainTex");
+            public static readonly int TempBuf           = P("_TempBuf");
+            public static readonly int Lut0              = P("_DepthColorGradingLut0");
+            public static readonly int Lut1              = P("_DepthColorGradingLut1");
+            public static readonly int Params            = P("_DepthColorGradingParams");
+            public static readonly int Params2           = P("_DepthColorGradingParams2");
+            public static readonly int Params3           = P("_DepthColorGradingParams3");
+            public static readonly int DepthToViewParams = P("_DepthColorGradingDepthToViewParams");
         }
 
 #if ENABLE_PROFILER
-        private const string ProfilingSamplerBlockName = "DepthColorGradingPass";
+        private const           string           ProfilingSamplerBlockName = "DepthColorGradingPass";
         private static readonly ProfilingSampler ProfilingSampler = new ProfilingSampler(ProfilingSamplerBlockName);
 #endif
 
-        private readonly Material _material;
-        private readonly DepthColorGradingLut2d _lut2d0 = new DepthColorGradingLut2d();
-        private readonly DepthColorGradingLut2d _lut2d1 = new DepthColorGradingLut2d();
+        private readonly Material               _material;
+        private readonly DepthColorGradingLut2d _lut0 = new DepthColorGradingLut2d();
+        private readonly DepthColorGradingLut2d _lut1 = new DepthColorGradingLut2d();
 
 //      public static DepthColorGradingPass Create() => new DepthColorGradingPass(RenderPassEvent.AfterRenderingOpaques);
 //      public static DepthColorGradingPass Create() => new DepthColorGradingPass(RenderPassEvent.BeforeRenderingPostProcessing);
-        public static DepthColorGradingPass Create() => new DepthColorGradingPass(RenderPassEvent.AfterRenderingPostProcessing);
+        public static DepthColorGradingPass Create()
+            => new DepthColorGradingPass(RenderPassEvent.AfterRenderingPostProcessing);
 
         private DepthColorGradingPass(RenderPassEvent renderPassEvent)
         {
@@ -74,7 +74,7 @@ namespace DepthColorGrading {
                 return;
             }
 
-            VolumeStack volumeStack = VolumeManager.instance.stack;
+            VolumeStack       volumeStack       = VolumeManager.instance.stack;
             DepthColorGrading depthColorGrading = volumeStack.GetComponent<DepthColorGrading>();
             if (depthColorGrading == null || !depthColorGrading.IsActive())
             {
@@ -86,17 +86,17 @@ namespace DepthColorGrading {
             if (universalAdditionalCameraData != null)
             {
                 // Require depth texture
-                universalAdditionalCameraData.requiresDepthOption = UnityEngine.Rendering.Universal.CameraOverrideOption.On;
+                universalAdditionalCameraData.requiresDepthOption = CameraOverrideOption.On;
             }
 
             {
-                float blend = depthColorGrading.blend.value;
+                float blend       = depthColorGrading.blend.value;
                 float skyBoxBlend = depthColorGrading.skyBoxBlend.value;
-                float far = Mathf.Pow(16384.0f, Mathf.Pow(depthColorGrading.falloff.value, 2.0f));
-                float falloff = -1.0f / (far * 0.5f);
-                float hdrScale = cameraData.isHdrEnabled ? 4.0f : 1.0f;
-                float lutWidth = _lut2d0.Width;
-                float lutHeight = _lut2d0.Height;
+                float far         = Mathf.Pow(16384.0f, Mathf.Pow(depthColorGrading.falloff.value, 2.0f));
+                float falloff     = -1.0f / (far * 0.5f);
+                float hdrScale    = cameraData.isHdrEnabled ? 4.0f : 1.0f;
+                float lutWidth    = _lut0.Width;
+                float lutHeight   = _lut0.Height;
 
                 float a = skyBoxBlend * blend;
                 float b = blend;
@@ -106,48 +106,48 @@ namespace DepthColorGrading {
                 float a2 = 1.0f / lutHeight;
                 float b2 = lutHeight - 1;
                 float c2 = falloff;
-                float d2 = 0;               // (unused)
+                float d2 = 0; // (unused)
 
-                float a3 = 0.5f / lutWidth;
-                float b3 = 0.5f / lutHeight;
-                float c3 = (lutHeight-1) / lutWidth;
-                float d3 = (lutHeight-1) / lutHeight;
+                float a3 = 0.5f            / lutWidth;
+                float b3 = 0.5f            / lutHeight;
+                float c3 = (lutHeight - 1) / lutWidth;
+                float d3 = (lutHeight - 1) / lutHeight;
 
-                _material.SetVector(ShaderId.Params, new Vector4(a, b, c, d));
+                _material.SetVector(ShaderId.Params,  new Vector4(a,  b,  c,  d));
                 _material.SetVector(ShaderId.Params2, new Vector4(a2, b2, c2, d2));
                 _material.SetVector(ShaderId.Params3, new Vector4(a3, b3, c3, d3));
             }
 
             {
-                Matrix4x4 c = camera.projectionMatrix;
-                float m00 = c[0,0];
-                float m02 = c[0,2];
-                float m11 = c[1,1];
-                float m20 = c[2,0];
-                float x = 2f / m00;
-                float y = (-1f - m02) / m00;
-                float z = 2f / m11;
-                float w = (-1f - m20) / m11;
+                Matrix4x4 c   = camera.projectionMatrix;
+                float     m00 = c[0, 0];
+                float     m02 = c[0, 2];
+                float     m11 = c[1, 1];
+                float     m20 = c[2, 0];
+                float     x   = 2f          / m00;
+                float     y   = (-1f - m02) / m00;
+                float     z   = 2f          / m11;
+                float     w   = (-1f - m20) / m11;
                 _material.SetVector(ShaderId.DepthToViewParams, new Vector4(x, y, z, w));
             }
 
             {
-                _lut2d0.h       = depthColorGrading.h0.value;
-                _lut2d0.s       = depthColorGrading.s0.value;
-                _lut2d0.l       = depthColorGrading.l0.value;
-                _lut2d0.gamma   = -depthColorGrading.gamma0.value;
-                _lut2d0.tint    = depthColorGrading.tint0.value;
-                _lut2d0.lift    = depthColorGrading.lift0.value;
+                _lut0.h     = depthColorGrading.h0.value;
+                _lut0.s     = depthColorGrading.s0.value;
+                _lut0.l     = depthColorGrading.l0.value;
+                _lut0.gamma = -depthColorGrading.gamma0.value;
+                _lut0.tint  = depthColorGrading.tint0.value;
+                _lut0.lift  = depthColorGrading.lift0.value;
 
-                _lut2d1.h       = depthColorGrading.h1.value;
-                _lut2d1.s       = depthColorGrading.s1.value;
-                _lut2d1.l       = depthColorGrading.l1.value;
-                _lut2d1.gamma   = -depthColorGrading.gamma1.value;
-                _lut2d1.tint    = depthColorGrading.tint1.value;
-                _lut2d1.lift    = depthColorGrading.lift1.value;
+                _lut1.h     = depthColorGrading.h1.value;
+                _lut1.s     = depthColorGrading.s1.value;
+                _lut1.l     = depthColorGrading.l1.value;
+                _lut1.gamma = -depthColorGrading.gamma1.value;
+                _lut1.tint  = depthColorGrading.tint1.value;
+                _lut1.lift  = depthColorGrading.lift1.value;
 
-                _material.SetTexture(ShaderId.Lut0, _lut2d0.Texture);
-                _material.SetTexture(ShaderId.Lut1, _lut2d1.Texture);
+                _material.SetTexture(ShaderId.Lut0, _lut0.Texture);
+                _material.SetTexture(ShaderId.Lut1, _lut1.Texture);
             }
 
             CommandBuffer cmd = CommandBufferPool.Get();
@@ -155,10 +155,10 @@ namespace DepthColorGrading {
             using (new ProfilingScope(cmd, ProfilingSampler))
 #endif
             {
-                var renderer = renderingData.cameraData.renderer;
-                RenderTextureDescriptor rtd = renderingData.cameraData.cameraTargetDescriptor;
+                ScriptableRenderer      renderer = cameraData.renderer;
+                RenderTextureDescriptor rtd      = cameraData.cameraTargetDescriptor;
                 rtd.depthBufferBits = 0;
-                rtd.msaaSamples = 1;
+                rtd.msaaSamples     = 1;
 
                 cmd.GetTemporaryRT(ShaderId.TempBuf, rtd, FilterMode.Bilinear);
                 cmd.Blit(source: renderer.cameraColorTarget, dest: ShaderId.TempBuf);
@@ -166,6 +166,7 @@ namespace DepthColorGrading {
                 cmd.Blit(source: ShaderId.TempBuf, dest: renderer.cameraColorTarget, _material, pass: 0);
                 cmd.ReleaseTemporaryRT(ShaderId.TempBuf);
             }
+
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
