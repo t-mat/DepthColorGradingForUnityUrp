823317a5ed978e46f2f93299ba7cd51f2ec0375b
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2021-12-15 12:13:40 +0900

    Update DepthColorGrading.cs

--- Assets/DepthColorGrading/DepthColorGrading.cs
+++ Assets/DepthColorGrading/DepthColorGrading.cs
@@ -8,21 +8,21 @@ using UnityEngine.Rendering.Universal;
 
 namespace DepthColorGrading {
     public sealed class DepthColorGrading : VolumeComponent, IPostProcessComponent {
-        public ClampedFloatParameter blend       = new ClampedFloatParameter(0f,   0f, 1f);
-        public ClampedFloatParameter skyBoxBlend = new ClampedFloatParameter(0.5f, 0f, 1f);
-        public ClampedFloatParameter falloff     = new ClampedFloatParameter(0.1f, 0f, 1f);
+        public ClampedFloatParameter blend       = new ClampedFloatParameter(value:0f,   min:0f, max:1f);
+        public ClampedFloatParameter skyBoxBlend = new ClampedFloatParameter(value:0.5f, min:0f, max:1f);
+        public ClampedFloatParameter falloff     = new ClampedFloatParameter(value:0.1f, min:0f, max:1f);
 
-        public ClampedFloatParameter h0     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter s0     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter l0     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter gamma0 = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter h0     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter s0     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter l0     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter gamma0 = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
         public ColorParameter        tint0  = new ColorParameter(Color.white);
         public ColorParameter        lift0  = new ColorParameter(Color.black);
 
-        public ClampedFloatParameter h1     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter s1     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter l1     = new ClampedFloatParameter(0f, -1f, 1f);
-        public ClampedFloatParameter gamma1 = new ClampedFloatParameter(0f, -1f, 1f);
+        public ClampedFloatParameter h1     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter s1     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter l1     = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
+        public ClampedFloatParameter gamma1 = new ClampedFloatParameter(value:0f, min:-1f, max:1f);
         public ColorParameter        tint1  = new ColorParameter(Color.white);
         public ColorParameter        lift1  = new ColorParameter(Color.black);
 
